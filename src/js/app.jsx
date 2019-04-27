fe8bfdb84be77e4c16ef3df9dab833593db88e65
Naoki Sawada <sawachon.21@gmail.com>
2019-04-28 03:32:58 +0900

    Follow hyperapp v2

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,28 +1,32 @@
 import { h, app } from "hyperapp";
-import { withLogger } from "@hyperapp/logger";
 
-const state = {
-  count: 0
-};
+const Increment = state => ({ ...state, count: state.count + 1 });
+const Decrement = state => ({ ...state, count: state.count - 1 });
+const Reset = state => ({ ...state, count: 0 });
 
-const actions = {
-  down: value => state => ({ count: state.count - value }),
-  up: value => state => ({ count: state.count + value })
-};
+const NewValue = (state, event) => ({ ...state, text: event.target.value });
 
-const CountContent = ({ count, up, down }) => (
-  <div class="count">
-    <h2>{count}</h2>
-    <button onclick={() => { down(1) }}>-</button>
-    <button onclick={() => { up(1) }}>+</button>
-  </div>
-);
+app({
+  init: () => ({
+    count: 0,
+    text: "",
+  }),
+  view: state => {
+    console.log(state);
 
-const view = (state, actions) => (
-  <div style={{ textAlign: "center" }}>
-    <h1>Hello, world!</h1>
-    <CountContent count={state.count} up={actions.up} down={actions.down} />
-  </div>
-);
-
-const main = withLogger(app)(state, actions, view, document.body);
+    return (
+      <div>
+        <div>
+          {state.count}
+          <button onclick={Increment}>+</button>
+          <button onclick={Decrement}>-</button>
+          <button onclick={Reset}>Reset</button>
+        </div>
+        <div>
+          <input type="text" value={state.text} oninput={NewValue} />
+        </div>
+      </div>
+    );
+  },
+  container: document.body,
+});
